65cfe4b3e93bea14cdd2e4d36c01c8b7bd43bb4f
AyushPandita <202201256@daiict.ac.in>
2024-11-22 04:10:44 +0530

    Updated rNewsProvider.js

--- backend/routes/rNewsProvider.js
+++ backend/routes/rNewsProvider.js
@@ -19,3 +19,5 @@ const upload = multer({ storage });
 
 router.get("/get_all_providers", getAllProviders);
 router.get("/get_following_providers", checkAuth, getFollowingProviders);
+router.post("/createchannel", checkAuth, upload.single('logo'), createChannel);
+router.get("/getchannels", checkAuth, getChannels);
