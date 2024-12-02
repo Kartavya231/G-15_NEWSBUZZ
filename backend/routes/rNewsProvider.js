fc89f0b4aa5f7d17a9d46c15489a823aee988419
AyushPandita111 <202201256@daiict.ac.in>
2024-12-02 18:49:42 +0530

    Minor fixes

--- backend/routes/rNewsProvider.js
+++ backend/routes/rNewsProvider.js
@@ -1,7 +1,11 @@
+// const router = require("express").Router();
+// const { getAllProviders, getFollowingProviders, createChannel, getChannels, deleteChannel } = require("../controllers/cprovider.js");
+// const checkAuth = require("../middleware/checkAuth.js");
+// const multer = require('multer');
+
 import express from "express";
 const router = express.Router();
-const { muteprovider, unmuteprovider } = require("../controllers/cmute.js");
-import { getAllProviders} from "../controllers/cNewsProvider.js";
+import { getAllProviders, getFollowingProviders, createChannel, getChannels, deleteChannel } from "../controllers/cNewsProvider.js";
 import checkAuth from "../middleware/checkAuth.js";
 import multer from 'multer';
 
@@ -20,8 +24,11 @@ const upload = multer({ storage });
 
 router.get("/get_all_providers", getAllProviders);
 router.get("/get_following_providers", checkAuth, getFollowingProviders);
+
 router.post("/createchannel", checkAuth, upload.single('logo'), createChannel);
+
 router.get("/getchannels", checkAuth, getChannels);
+
 router.delete("/deletechannel/:id", checkAuth, deleteChannel);
-router.get("/mute/:id",muteprovider)
-router.get("/unmute/:id",unmuteprovider)
+
+export default router;;
